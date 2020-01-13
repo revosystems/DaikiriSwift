4fe485054822900a9cf89507deca5df78c4e471c
Jordi Puigdellívol <jordi.p@revo.works>
2020-01-13 12:34:23 +0100

    Initial Commit

---
+++ DaikiriSwiftTests/DaikiriSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  DaikiriSwiftTests.swift
+//  DaikiriSwiftTests
+//
+//  Created by Jordi Puigdellívol on 13/01/2020.
+//  Copyright © 2020 Jordi Puigdellívol. All rights reserved.
+//
+
+import XCTest
+@testable import DaikiriSwift
+
+class DaikiriSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
