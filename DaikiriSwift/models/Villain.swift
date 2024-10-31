0b9f62773b4aed7f70877bd6530024c95ddba456
Jordi Puigdellívol <jordi.p@revo.works>
2024-10-31 08:24:12 +0100

    working with managed

--- DaikiriSwift/models/Villain.swift
+++ DaikiriSwift/models/Villain.swift
@@ -4,12 +4,19 @@ class Villain {
 
     //var managed:NSManagedObject
     
+    var id:Int!
     
     static func fetch() throws {
         let request = NSFetchRequest<NSManagedObject>(entityName: "Hero")
-        let result = try DaikiriCoreData.manager.context.fetch(request)
+        let result = try DaikiriCoreData.manager.context.fetch(request).first!
         
         print(result)
+
+        let villain = Villain()
+        villain.id = result.value(forKey: "id") as? Int
+        
+        
+        print(villain)
         
     }
     
