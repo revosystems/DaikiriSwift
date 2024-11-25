aa56bbd5a55350db0384509cfce60886fec8cb1f
Sergi Baró <sergi.baro@revo.works>
2024-11-25 15:32:53 +0100

    minor change

--- DaikiriSwift/src/extensions/NSManaged+daikiri.swift
+++ DaikiriSwift/src/extensions/NSManaged+daikiri.swift
@@ -37,15 +37,3 @@ extension NSManagedObject {
         return value is Encodable
     }
 }
-
-    
-    /*
-    func toJson() throws -> Data {
-        var json: [String:Any] = [:]
-        
-        entity.attributesByName.forEach { (key: String, value: NSAttributeDescription) in
-            json[key] = self.value(forKey: key)
-        }
-        return try JSONSerialization.data(withJSONObject: json, options: [])
-    }*/
-
