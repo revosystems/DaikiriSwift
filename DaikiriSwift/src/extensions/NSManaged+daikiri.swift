98b06ca8156146a74750747eecd99584bbe45bf8
Sergi Baró <sergi.baro@revo.works>
2024-11-25 15:31:27 +0100

    feat: encode correctly custom econdable types.

--- DaikiriSwift/src/extensions/NSManaged+daikiri.swift
+++ DaikiriSwift/src/extensions/NSManaged+daikiri.swift
@@ -1,7 +1,45 @@
 import CoreData
 
 extension NSManagedObject {
+    func toJson() throws -> Data {
+        var json: [String: Any] = [:]
+
+        try entity.attributesByName.forEach { (key: String, value: NSAttributeDescription) in
+            let fieldValue = self.value(forKey: key)
+            
+            if isCustomEncodable(fieldValue) {
+                let encodableValue = fieldValue as! Encodable
+                let encodedData = try JSONEncoder().encode(AnyEncodable(encodableValue))
+                let encodedJson = try JSONSerialization.jsonObject(with: encodedData, options: [])
+                json[key] = encodedJson
+                return
+            }
+            
+            json[key] = fieldValue
+        }
+
+        return try JSONSerialization.data(withJSONObject: json, options: [])
+    }
+
+    private func isCustomEncodable(_ value: Any) -> Bool {
+        let foundationTypes: [Any.Type] = [
+            String.self, Bool.self, Double.self, Float.self,
+            Int.self, Int8.self, Int16.self, Int32.self, Int64.self,
+            UInt.self, UInt8.self, UInt16.self, UInt32.self, UInt64.self,
+            Array<Any>.self, Dictionary<String, Any>.self,
+            Date.self, Data.self
+        ]
+        
+        if foundationTypes.contains { $0 == type(of: value) } {
+            return false
+        }
+
+        return value is Encodable
+    }
+}
+
     
+    /*
     func toJson() throws -> Data {
         var json: [String:Any] = [:]
         
@@ -9,5 +47,5 @@ extension NSManagedObject {
             json[key] = self.value(forKey: key)
         }
         return try JSONSerialization.data(withJSONObject: json, options: [])
-    }
-}
+    }*/
+
--- DaikiriSwift/src/propertyWrappers/NonCodable.swift
+++ DaikiriSwift/src/propertyWrappers/NonCodable.swift
@@ -22,3 +22,16 @@ extension NonCodable: Codable {
         self.value = nil
     }
 }
+
+struct AnyEncodable: Encodable {
+    private let _encode: (Encoder) throws -> Void
+
+    init<T: Encodable>(_ wrapped: T) {
+        _encode = wrapped.encode(to:)
+    }
+
+    func encode(to encoder: Encoder) throws {
+        try _encode(encoder)
+    }
+}
+
